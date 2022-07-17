bf42dfb76a2647c446bc877bc670a7470cadf778
Fatima Tuz Zehra <57491064+fatimacodes2001@users.noreply.github.com>
2022-07-17 15:51:59 +0500

    added final touches

--- src/Components/Greeting.js
+++ src/Components/Greeting.js
@@ -7,6 +7,15 @@ import emoji from "react-easy-emoji";
 import image from "../Assets/fatima.jpg"
 
 export default function Greeting() {
+  function onClickHire(){
+    window.open(greeting.upwork_link, "_blank")
+
+  }
+
+  function onClickResume(){
+    window.open(greeting.upwork_link, "_blank")
+
+  }
   return (
     <Fade bottom duration={1000} distance="40px">
       <div className='greet' id="greeting">
@@ -15,8 +24,8 @@ export default function Greeting() {
         <p className='greeting-sub'>{greeting.detail}</p>
         <SocialMedia />
         <div className='greeting-buttons'>
-          <a className='greeting-link' href={greeting.upwork_link}><button className='greeting-button'>HIRE ME</button></a>
-          <a className='greeting-link' href={greeting.resume_link}><button className='greeting-button'>SEE MY RESUME</button></a>
+          <button className='greeting-button' onClick={onClickHire}>HIRE ME</button>
+          <button className='greeting-button' onClick={onClickResume}>SEE MY RESUME</button>
         </div>
         
       </div>
--- src/Components/SocialMedia.js
+++ src/Components/SocialMedia.js
@@ -6,16 +6,16 @@ export default function SocialMedia() {
   return (
     <div className='social-buttons'>
         
-        <a className='icon-button github' href={social_media.github}>
+        <a className='icon-button github' target="_blank" href={social_media.github}>
         <i className="fab fa-github"></i>
         </a>
-        <a className='icon-button linkedin' href={social_media.linkedin}>
+        <a className='icon-button linkedin' target="_blank" href={social_media.linkedin}>
         <i className="fab fa-linkedin-in"></i>
         </a>
-        <a className='icon-button mail' href={social_media.mail}>
+        <a className='icon-button mail' target="_blank" href={social_media.mail}>
         <i className="fas fa-envelope"></i>
         </a>
-        <a className='icon-button facebook'href={social_media.facebook}>
+        <a className='icon-button facebook' target="_blank" href={social_media.facebook}>
         <i className="fab fa-facebook-f"></i>
         </a>
     </div>
